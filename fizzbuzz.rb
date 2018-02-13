564137c785c711f136dc992bba81230f14b0270b
Azusa Uchikura <azusa.uchikura@wework.com>
2018-02-13 04:17:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,2 @@
 def fizzbuzz(number)
 end
-
-if number % 3 == 0 && number % 5 == 0
-  puts "FizzBuzz"
- elsif number % 3 = 0 
-    puts "Fizz"
- elsif number % 5 = 0 
-    puts "Buzz"
- else number
-end
