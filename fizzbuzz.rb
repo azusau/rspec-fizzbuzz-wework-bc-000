9762a2bba18efa2a5615a4e483dd6f9e093f8759
Azusa Uchikura <azusa.uchikura@wework.com>
2018-02-13 04:19:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,14 +12,3 @@ end
   end
  else int
 end
-
- if int % 3 == 0 && int % 5 != 0
-    "Fizz"
-	end
-  elsif int % 5 == 0 && int %3 != 0 
-    "Buzz"
-	end
-  elsif int % 3 == 0 && int %5 == 0
-    "Fizzbuzz"
-  end
-end
